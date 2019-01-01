9187b96091891cd3aedd7ef3b8b49135eadc4835
FixingMind5 <aguilarmanu5@gmail.com>
2019-01-01 15:45:42 -0600

    Agregué la clase DEBIT y GAIN

--- MisFinanzas.playground/Contents.swift
+++ MisFinanzas.playground/Contents.swift
@@ -6,6 +6,35 @@ protocol addDeuda {
     func addDeuda(_ deuda: DEUDA)
 }
 
+protocol transactions {
+    var transName: String { get set }
+    var transValue: Float { get set }
+}
+
+protocol gainTypes : transactions {
+    var gainType: gainCategory { get }
+}
+
+protocol debitTypes : transactions {
+    var debitType: debitCategory { get }
+}
+
+enum gainCategory : String {
+    case salary = "Sueldo"
+    case bienes = "Cosas que tenemos"
+    case deuda = "Deudas que tienen conmigo"
+}
+
+enum debitCategory : String {
+    case ahorro = "Ahorros"
+    case vivienda = "Vivienda"
+    case transporte = "Pasajes"
+    case ocio = "Ocio"
+    case educacion = "Escuela"
+    case necesidades = "Cuidado personal"
+    case trabajo = "Trabajo"
+}
+
 class PERSONA {
     var name: String
     var lastName: String
@@ -60,6 +89,30 @@ class DEUDA {
     }
 }
 
+class GAIN : gainTypes {
+    var gainType: gainCategory
+    var transName: String
+    var transValue: Float
+    
+    init(transName: String, transValue: Float, gainType: gainCategory) {
+        self.transName = transName
+        self.transValue = transValue
+        self.gainType = gainType
+    }
+}
+
+class DEBIT : debitTypes {
+    var debitType: debitCategory
+    var transName: String
+    var transValue: Float
+    
+    init(transName: String, transValue: Float, debitType: debitCategory) {
+        self.transName = transName
+        self.transValue = transValue
+        self.debitType = debitType
+    }
+}
+
 extension PERSONA : addActivo {
     func addActivo(_ activo: ACTIVO) {
         activos.append(activo)
@@ -75,3 +128,9 @@ extension PERSONA : addDeuda {
 var manu = PERSONA(name: "Manuel", lastName: "Aguilar")
 
 manu.Cuenta = CUENTA(saldoCuenta: 500)
+
+let ropa = ACTIVO(
+    valorActivo: 1_000,
+    liquidez: 1,
+    productividad: true
+)
